b783aabf0da5d4f7238cb0d620296b8f27d1a7ee
abojarski <abojarski@student.ethz.ch>
2024-09-25 11:11:18 +0200

    improve logging

--- src/Hercules.py
+++ src/Hercules.py
@@ -10,25 +10,21 @@ class Hercules:
         self.destination_dir = config.rth_target_dir
 
     def transfer(self, file: File):
-        print(f"HERCULES: SENDING file {file.name} ({file.local_path}) to {self.destination_dir}")
         infile = file.local_path
         outfile = self.destination_dir + "/" + file.name
         resp = requests.get(
             f"http://{self.hercules_monitor_address}/submit?file={infile}&destfile={outfile}&dest={self.destination_address}"
         ).text
-        print("Hercules Response:", resp)
         file.hercules_file_id = [int(s) for s in resp.split() if s.isdigit()][0]
-        print("hercules file id: ", file.hercules_file_id)
+        print(
+            f"HERCULES: SENDING file {file.name} ({file.local_path}) to {self.destination_dir} with file id {file.hercules_file_id}"
+        )
 
     def status(self, file: File):
         if file.hercules_file_id == -1:
             FileStatus.ERROR
-        resp = requests.get(
-            f"http://{self.hercules_monitor_address}/status?id={file.hercules_file_id}"
-        ).text
-        print("Hercules Status Response:", resp)
+        resp = requests.get(f"http://{self.hercules_monitor_address}/status?id={file.hercules_file_id}").text
         status = [int(s) for s in resp.split() if s.isdigit()][0]
-        print(f"Current status: {status}")
         if status == 3:
             print(f"Hercules transfer for {file.name} done.")
             return FileStatus.SENT
